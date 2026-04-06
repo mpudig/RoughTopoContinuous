5acd1c9a41c5aad310703ba7fc6aa3862dc10101
Matt Pudig <mp6191@torch-login-b-0.hpc-infra.svc.cluster.local>
2026-04-06 16:04:57 -0400

    Add topographic transfer term.

--- MultiLevelQG/Helpers/energy_budget.jl
+++ MultiLevelQG/Helpers/energy_budget.jl
@@ -3,16 +3,15 @@
 module EnergyBudget
 
 # compile other packages
-using GeophysicalFlows, FFTW, Statistics, Random, CUDA, CUDA_Driver_jll, CUDA_Runtime_jll, GPUCompiler
+using GeophysicalFlows, FFTW, Statistics, Random, CUDA, CUDA_Driver_jll, CUDA_Runtime_jll, GPUCompiler, LinearAlgebra
 
 """
 	KETransfer(prob)
 
-Returns KE transfer in spectral space, binned into isotropic wavenumber.
+Returns KE transfer in spectral space, integrated azimuthally.
 """
 function KETransfer(prob)
     sol, vars, params, grid = prob.sol, prob.vars, prob.params, prob.grid
-    MultiLevelQG.updatevars!(vars, params, grid, sol)
 
     # Grid
     A = device_array(grid.device)
@@ -45,7 +44,7 @@ function KETransfer(prob)
     MultiLevelQG.fwdtransform!(Fh, F, params)
     @. T_ke += real(conj(vars.ψh) * (im * grid.l * Fh))
 
-    # Average in rings
+    # Integrate azimuthally
     kr = grid.kr
     l = grid.l
     Kr = @. sqrt(kr^2 + l^2)
@@ -54,15 +53,72 @@ function KETransfer(prob)
     dK = 2 * pi / Lx
     nK = floor(Int, (Kmax - Kmin) / dK)
 
-    Kr_flat = vec(Kr)                                                   # (nkr * nl,)
-    iK_flat = clamp.(floor.(Int, (Kr_flat .- Kmin) ./ dK) .+ 1, 1, nK)  # (nkr * nl,)
-    W = T.(iK_flat' .== A(1 : nK))                                      # (nK, nkr * nl)
-    counts = vec(sum(W, dims = 2))                                      # (nK,)
-    W ./= max.(reshape(counts, nK, 1), 1)                               # normalise rows
+    Kr_flat = vec(Kr)                                                    # (nkr * nl,)
+    iK_flat = clamp.(floor.(Int, (Kr_flat .- Kmin) ./ dK) .+ 1, 1, nK)   # (nkr * nl,)
+    W = T.(iK_flat' .== A(1 : nK))                                       # (nK, nkr * nl)
+    counts = vec(sum(W, dims = 2))                                       # (nK,)
+    W ./= max.(reshape(counts, nK, 1), 1)                                # normalise rows
 
-    T_ke_iso = Array(W * reshape(T_ke, nkr * nl, nz))                   # (nK, nz)
+    K_bins = Kmin .+ (0.5 : nK) .* dK                                    # bin centres (nK,)
+    T_ke_iso = 2 * π .* K_bins .* Array(W * reshape(T_ke, nkr * nl, nz)) # (nK, nz)
     
     return T_ke_iso
+end
+
+"""
+	TopoTransfer(prob)
+
+Returns topographic transfer KE budget term in spectral space, integrated azimuthally.
+"""
+function TopoTransfer(prob)
+    sol, vars, params, grid = prob.sol, prob.vars, prob.params, prob.grid
+
+    # Grid
+    A = device_array(grid.device)
+    T = eltype(grid)
+    Lx = grid.Lx
+    nx = grid.nx
+    nkr = grid.nkr
+    nl = grid.nl
+    nz = params.nlevels
+
+    ### Calculate
+    # Assign
+    wh = A(zeros(Complex{T}, nkr, nl, nz))     # spectral space vertical velocity (nkr, nl, nz)
+    rhsh = A(zeros(Complex{T}, nkr, nl, nz))   # spectral space vertical velocity interior forcing + boundary conditions (nkr, nl, nz)
+
+    # Vertical velocity
+    @views rhsh[:, :, end] .= im * grid.kr .* rfft(vars.u[:, :, end] .* params.eta) .+
+                              im * grid.l  .* rfft(vars.v[:, :, end] .* params.eta)
+
+    MultiLevelQG.omegaeqn!(wh, rhsh, params, grid)
+
+    # Topographic transfer
+    D_wh = reshape(wh, nkr * nl, nz)
+    mul!(D_wh, copy(D_wh), params.D')
+    wh .= reshape(D_wh, nkr, nl, nz)
+
+    T_topo = -params.f₀ * real(conj.(vars.ψh) .* wh)
+
+    # Integrate azimuthally
+    kr = grid.kr
+    l = grid.l
+    Kr = @. sqrt(kr^2 + l^2)
+    Kmin = 0.
+    Kmax = sqrt(maximum(kr)^2 + maximum(abs.(l))^2)
+    dK = 2 * pi / Lx
+    nK = floor(Int, (Kmax - Kmin) / dK)
+
+    Kr_flat = vec(Kr)                                                    # (nkr * nl,)
+    iK_flat = clamp.(floor.(Int, (Kr_flat .- Kmin) ./ dK) .+ 1, 1, nK)   # (nkr * nl,)
+    W = T.(iK_flat' .== A(1 : nK))                                       # (nK, nkr * nl)
+    counts = vec(sum(W, dims = 2))                                       # (nK,)
+    W ./= max.(reshape(counts, nK, 1), 1)                                # normalise rows
+
+    K_bins = Kmin .+ (0.5 : nK) .* dK                                    # bin centres (nK,)
+    T_topo_iso = 2 * π .* K_bins .* Array(W * reshape(T_topo, nkr * nl, nz)) # (nK, nz)
+    
+    return T_topo_iso
 end
 
 
